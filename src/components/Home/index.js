b114b088232184417a07e9c895dec63de662e464
Jack Knox <knox36@purdue.edu>
2024-10-21 20:02:46 -0400

    Make small formatting adjustments #13

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,7 +9,7 @@ import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ['a', 'c', 'k', ' ','K', 'n', 'o', 'x']
+  const nameArray = ['a', 'c', 'k', ' ','K', 'n', 'o', 'x', ',']
   const jobArray = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
 
   useEffect(() => {
