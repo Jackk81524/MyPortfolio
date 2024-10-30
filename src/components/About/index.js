0995e6083aec522647e1aa4a609ba8d98ea41746
Jack Knox <knox36@purdue.edu>
2024-10-29 20:25:30 -0400

    Fix container resizing issue, along with other contact page fixes. #16

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,6 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
-import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Loader from 'react-loaders'
 
@@ -28,37 +27,14 @@ const About = () => {
                         />
                     </h1>
                     <p>
-                        Hi, I’m Jack Knox! I graduated from Purdue University in May 2024 with a Bachelor’s degree in Computer Engineering, and I currently work as an Applications Developer. I am from Long Island, New York, and I love to travel, stay active, and listen to music.
+                        Hi, I’m Jack Knox! I graduated from Purdue University in May 2024 with a Bachelor’s degree in Computer Engineering, and I currently work as an Applications Developer. I am from Long Island, New York, and I love to travel, stay active, and watch baseball.
                     </p>
                     <p>
-                        I’ve gained experience with a wide range of programming languages and frameworks, specializing in C#, .NET, C, C++, TypeScript, Angular, React, JavaScript, Python, and Flask. This website itself is built with JavaScript and React—a project I took on to showcase my skills and creativity!
+                        I’ve gained experience with a wide range of programming languages and frameworks, specializing in C#, .NET, C, C++, TypeScript, Angular, React, JavaScript, Python, Flask, and Swift. This website itself is built with JavaScript and React—a project I took on to showcase my skills and creativity!
                     </p>
                     <p>
                         My passion for technology’s rapid evolution inspired me to become a software developer, driving me to continually learn and grow in this dynamic field. I strive to stay on the cutting edge by mastering new frameworks, tackling challenging projects, and exploring emerging trends. With each step, I aim to hone my craft and make a meaningful impact on the world through innovative technology.
                     </p>
-                </div>
-
-                <div className="stage-cube-cont">
-                    <div className="cubespinner">
-                        <div className="face1">
-                        <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-                        </div>
-                        <div className="face2">
-                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-                        </div>
-                        <div className="face3">
-                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                        </div>
-                        <div className="face4">
-                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-                        </div>
-                        <div className="face5">
-                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-                        </div>
-                        <div className="face6">
-                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
-                        </div>
-                    </div>
                 </div>
             </div>
             <Loader type="pacman" />
