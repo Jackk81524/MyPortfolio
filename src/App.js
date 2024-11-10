8115f4e77b905ce5c2257fe7dfc30a63ad089381
Jack Knox <knox36@purdue.edu>
2024-11-10 17:54:26 -0500

    Disbale on mobile

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contacts';
-
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkDevice = () => {
+      const userAgent = navigator.userAgent.toLowerCase();
+      const mobileDevices = /iphone|ipod|android|windows phone|blackberry|iemobile|mobile|opera mini/;
+      setIsMobile(mobileDevices.test(userAgent));
+    };
+
+    checkDevice();
+    window.addEventListener('resize', checkDevice);
+
+    return () => {
+      window.removeEventListener('resize', checkDevice);
+    };
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div className='Mobile'>
+        <p>Sorry, at this time this website is only available on a desktop device. I am working to add mobile functionality in the future.</p>
+      </div>
+    );
+  }
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />} >
           <Route index element={<Home />} />
-          <Route path = "about" index element={<About />} />
-          <Route path = "contact" index element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
